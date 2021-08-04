9cd70a3df1d2d47e2cad029bbb1e24ef7207ef64
furxx2000 <furxx2000@gmail.com>
2021-08-05 01:48:36 +0800

    新增main區塊

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,15 @@ export default function Home() {
 
       <Header />
       <Banner />
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+
+          {/* Pull some data from a server - API endpoints */}
+        </section>
+      </main>
     </div>
   );
 }
+
+export async function getStaticProps() {}
