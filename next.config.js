9338c03d3302159d946c44f03d24508997428343
furxx2000 <furxx2000@gmail.com>
2021-08-10 00:05:44 +0800

    完成 Airbnb Challenge day4

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
   images: {
     domains: ["links.papareact.com"],
   },
+  env: {
+    mapbox_key:
+      "pk.eyJ1IjoiZnVyeHgyMDAwIiwiYSI6ImNrczRxdzV4eDB1cXgycG5qN3l5NDdkYXMifQ.pVFOKJbLU62g6zDYqRp7mQ",
+  },
 };
